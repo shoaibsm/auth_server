41c3fcfeff1aefcd2ae32cf1830bc4f6ec6cdd46
shoaib665583 <shoaib@cablesmith.in>
2024-04-03 02:26:30 +0530

    url changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const { dbConnect } = require('./dbConnect');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const originUrl = process.env.ORIGIN_URL
 
 dotenv.config()
 
@@ -15,14 +16,15 @@ app.use(express.json())
 app.use(morgan('common'))
 app.use(cookieParser());
 
+app.use(cors({
+    credentials: true,
+    origin: originUrl
+}))
+app.use('/', mainRouter)
+
 // app.get('/', (req, res) => {
 //     res.send('Ok from server')
 // })
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-}))
-app.use('/', mainRouter)
 
 dbConnect()
 app.listen(4000, () => {
